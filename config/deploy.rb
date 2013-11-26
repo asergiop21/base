c66513542110a3be71505bb0670c6ad4e0fe401e
Sergio <asergiop21@gmail.com>
2013-11-26 17:44:18 -0300

    cambio de nombre

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'bundler/capistrano'
 set :application, "base_gestion"
 set :repository,  "git@github.com:asergiop21/base.git" #https://github.com/asergiop21/base.git"
 server "10.0.100.52", :web, :app, :db, primary: true
-set :deploy_to, '/home/alejandro/base'
+set :deploy_to, '/home/alejandro/base_g'
 #set :deploy_to, '/home/sergio/proyecto_base'
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 set :user, 'alejandro'
@@ -12,9 +12,9 @@ set :branch, 'master'
 #set :deploy_via, :copy
 #default_run_options[:pty] = true
 #ssh_options[:forward_agent] = true
-role :web, 'base.com'                          # Your HTTP server, Apache/etc
-role :app, 'base.com'                          # This may be the same as your `Web` server
-role :db, 'base.com', :primary => true # This is where Rails migrations will run
+role :web, 'base_gestion.com'                          # Your HTTP server, Apache/etc
+role :app, 'base_gestion.com'                          # This may be the same as your `Web` server
+role :db, 'base_gestion.com', :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
