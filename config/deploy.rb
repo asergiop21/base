6b54e63462d2521127bde103eb95cf8748de5acf
Sergio <asergiop21@hotmail.com>
2014-07-02 10:04:18 -0300

    add bundle exec deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ end
 desc "Migrate Production Database"
 task :migrate do
         puts "\n\n=== Migrating the Production Database! ===\n\n"
-        run "cd #{current_path}; rake db:migrate RAILS_ENV=production"
+        run "cd #{current_path}; bundle exec rake db:migrate RAILS_ENV=production"
         system "cap deploy:set_permissions"
 end
 end
